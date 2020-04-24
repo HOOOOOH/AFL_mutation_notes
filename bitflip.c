7c0ed2837563df7120dde3c8bd0ea05ff0b71153
HOOOOOH <36353749+HOOOOOH@users.noreply.github.com>
2020-04-24 12:14:02 +0800

    Update bitflip.c

--- bitflip.c
+++ bitflip.c
@@ -8,7 +8,7 @@
     _arf[(_bf) >> 3] ^= (128 >> ((_bf) & 7)); \
   } while (0)
 
-  /* Single walking bit. 一个bit步长 */
+  /* 一个bit步长 */
 
   stage_short = "flip1";
   stage_max   = len << 3;
@@ -75,9 +75,7 @@
 
       } else if (cksum != prev_cksum) {
 
-        /* Otherwise, if the checksum has changed, see if we have something
-           worthwhile queued up, and collect that if the answer is yes.
-           否则，如果校验和已更改，查看是否有值得排队的内容，如果答案为是，收集该值 */
+        /* 否则，如果校验和已更改，查看是否有值得排队的内容，如果答案为是，收集该值 */
 
         if (a_len >= MIN_AUTO_EXTRA && a_len <= MAX_AUTO_EXTRA)
           maybe_add_auto(a_collect, a_len);
@@ -87,9 +85,7 @@
 
       }
 
-      /* Continue collecting string, but only if the bit flip actually made
-         any difference - we don't want no-op tokens. 
-         继续收集字符串，但前提是位翻转实际上起到了任何作用-我们不想要无操作token*/
+      /* 继续收集字符串，但前提是位翻转实际上起到了任何作用-我们不想要无操作token */
 
       if (cksum != queue_cur->exec_cksum) {
 
@@ -107,7 +103,7 @@
   stage_finds[STAGE_FLIP1]  += new_hit_cnt - orig_hit_cnt;
   stage_cycles[STAGE_FLIP1] += stage_max;
 
-  /* Two walking bits. 两个bit步长 */
+  /* 两个bit步长 */
 
   stage_name  = "bitflip 2/1";
   stage_short = "flip2";
@@ -134,7 +130,7 @@
   stage_finds[STAGE_FLIP2]  += new_hit_cnt - orig_hit_cnt;
   stage_cycles[STAGE_FLIP2] += stage_max;
 
-  /* Four walking bits. 四个bit步长 */
+  /* 四个bit步长 */
 
   stage_name  = "bitflip 4/1";
   stage_short = "flip4";
@@ -165,12 +161,10 @@
   stage_finds[STAGE_FLIP4]  += new_hit_cnt - orig_hit_cnt;
   stage_cycles[STAGE_FLIP4] += stage_max;
 
-  /* Effector map setup. These macros calculate:
-
-     EFF_APOS      - position of a particular file offset in the map.
-     EFF_ALEN      - length of a map with a particular number of bytes.
-     EFF_SPAN_ALEN - map span for a sequence of bytes.
-     设置effector map,计算三个参数EFF_APOS、EFF_ALEN和EFF_SPAN_ALEN。
+  /* 设置effector map,计算三个参数EFF_APOS、EFF_ALEN和EFF_SPAN_ALEN。
+     EFF_APOS      - map中特定文件的偏移的位置
+     EFF_ALEN      - 具有特定字节数的map的长度.
+     EFF_SPAN_ALEN - map范围为字节序列.
    */
 
 #define EFF_APOS(_p)          ((_p) >> EFF_MAP_SCALE2)
@@ -178,9 +172,7 @@
 #define EFF_ALEN(_l)          (EFF_APOS(_l) + !!EFF_REM(_l))
 #define EFF_SPAN_ALEN(_p, _l) (EFF_APOS((_p) + (_l) - 1) - EFF_APOS(_p) + 1)
 
-  /* Initialize effector map for the next step (see comments below). Always
-     flag first and last byte as doing something. 
-     初始化 effector map 以进行下一步（请参阅下面的评论）。 
+  /* 初始化 effector map 以进行下一步（请参阅下面的评论）。 
      始终将第一个和最后一个字节标记为正在执行操作 */
 
   eff_map    = ck_alloc(EFF_ALEN(len));
@@ -191,7 +183,7 @@
     eff_cnt++;
   }
 
-  /* Walking byte. 一字节为步长 */
+  /* 一字节为步长 */
 
   stage_name  = "bitflip 8/8";
   stage_short = "flip8";
@@ -207,22 +199,16 @@
 
     if (common_fuzz_stuff(argv, out_buf, len)) goto abandon_entry;
 
-    /* We also use this stage to pull off a simple trick: we identify
-       bytes that seem to have no effect on the current execution path
-       even when fully flipped - and we skip them during more expensive
-       deterministic stages, such as arithmetics or known ints. 
-       我们还使用此阶段来完成一个简单的技巧：
+    /* 我们还使用此阶段来完成一个简单的技巧：
        我们确定即使完全翻转也似乎对当前执行路径没有影响的字节
-       —— 并在更高代价的确定性阶段（例如算术或已知整数）跳过它们*/
+       —— 并在更高代价的确定性阶段（例如算术或已知整数）跳过它们 */
 
     if (!eff_map[EFF_APOS(stage_cur)]) {
 
       u32 cksum;
 
-      /* If in dumb mode or if the file is very short, just flag everything
-         without wasting time on checksums. 
-         如果处于dumb模式或文件很短，
-         则只需标记所有内容而不会浪费时间在校验和上*/
+      /* 如果处于dumb模式或文件很短，
+         则只需标记所有内容而不会浪费时间在校验和上 */
 
       if (!dumb_mode && len >= EFF_MIN_LEN)
         cksum = hash32(trace_bits, MAP_SIZE, HASH_CONST);
@@ -240,11 +226,8 @@
 
   }
 
-  /* If the effector map is more than EFF_MAX_PERC dense, just flag the
-     whole thing as worth fuzzing, since we wouldn't be saving much time
-     anyway. 
-     如果effector map的dense大于EFF_MAX_PERC，
-     则只需将整个事情标记为模糊测试即可，因为无论如何我们都不会节省太多时间*/
+  /* 如果effector map的dense大于EFF_MAX_PERC，
+     则只需将整个东西标记为值得fuzzing即可，因为无论如何我们都不会节省太多时间 */
 
   if (eff_cnt != EFF_ALEN(len) &&
       eff_cnt * 100 / EFF_ALEN(len) > EFF_MAX_PERC) {
@@ -266,7 +249,7 @@
   stage_finds[STAGE_FLIP8]  += new_hit_cnt - orig_hit_cnt;
   stage_cycles[STAGE_FLIP8] += stage_max;
 
-  /* Two walking bytes. 双字步长 */
+  /* 双字步长 */
 
   if (len < 2) goto skip_bitflip;
 
@@ -279,7 +262,7 @@
 
   for (i = 0; i < len - 1; i++) {
 
-    /* Let's consult the effector map... 查阅effect map */
+    /* 查阅effect map */
 
     if (!eff_map[EFF_APOS(i)] && !eff_map[EFF_APOS(i + 1)]) {
       stage_max--;
@@ -305,7 +288,7 @@
 
   if (len < 4) goto skip_bitflip;
 
-  /* Four walking bytes. 四字节步长 */
+  /* 四字节步长 */
 
   stage_name  = "bitflip 32/8";
   stage_short = "flip32";
@@ -316,7 +299,7 @@
 
   for (i = 0; i < len - 3; i++) {
 
-    /* Let's consult the effector map... 查阅effect map */
+    /* 查阅effect map */
     if (!eff_map[EFF_APOS(i)] && !eff_map[EFF_APOS(i + 1)] &&
         !eff_map[EFF_APOS(i + 2)] && !eff_map[EFF_APOS(i + 3)]) {
       stage_max--;
